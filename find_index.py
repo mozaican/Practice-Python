5a4f468d0d1c119457e5730e215015ecb59aae64
oana.buturas <oana.buturas@gmail.com>
2018-07-13 20:43:46 +0300

    find the lowest index at which a value should be inserted

--- find_index.py
+++ find_index.py
@@ -1,6 +1,6 @@
 """
 Return the lowest index at which a value (second argument) 
-should be inserted into an array (first argument) once it 
+should be inserted into a list (first argument) once it 
 has been sorted. The returned value should be a number.
 """
 
